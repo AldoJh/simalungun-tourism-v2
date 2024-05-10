80463c6f045b5dcfe2c30a5f99b162881e383178
Fajar Rivaldi Chan <fajar.200170188@mhs.unimal.ac.id>
2024-05-10 23:30:22 +0700

    feat: statistic restaurant visitor

--- app/Http/Controllers/Web/Front/RestaurantController.php
+++ app/Http/Controllers/Web/Front/RestaurantController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use App\Models\RestaurantViewer;
 use App\Http\Controllers\Controller;
 use App\Models\RestaurantReview;
+use App\Models\RestaurantVisitor;
 use Illuminate\Support\Facades\Auth;
 
 class RestaurantController extends Controller
@@ -24,8 +25,29 @@ class RestaurantController extends Controller
         return view('front.pages.restaurant.index',  $data);
     }
 
-    public function show($slug) {
+    public function show($slug, Request $request) {
         $restaurant =  Restaurant::where('slug', $slug)->first();
+
+         // Ambil tahun dari request jika tersedia, atau gunakan sekarang sebagai default
+         $tahun = $request->input('tahun', date('Y'));
+         $visitorData = RestaurantVisitor::where('restaurant_id', $restaurant->id)
+             ->whereYear('date', $tahun)
+             ->get();
+         $monthlyVisitor = [];
+         for ($i = 1; $i <= 12; $i++) {
+             $monthlyVisitor[$i] = 0;
+         }
+         foreach ($visitorData as $visitor) {
+             $month = date('n', strtotime($visitor->date)); 
+             $monthlyVisitor[$month] += (int)$visitor->visitor;
+         }
+         $result = array_values($monthlyVisitor);
+ 
+         // Array 5 tahun kebelakang
+         $fiveYearsAgo = [];
+         for ($i = 0; $i < 5; $i++) {
+             $fiveYearsAgo[] = (int)date('Y', strtotime("-$i year"));
+         }
 
         $viewer = New RestaurantViewer();
         $viewer->restaurant_id = $restaurant->id;
@@ -37,7 +59,9 @@ class RestaurantController extends Controller
             'subTitle' => null,
             'page_id' => null,
             'restaurant' => $restaurant,
-            'review' => RestaurantReview::where('restaurant_id', $restaurant->id)->orderBy('created_at', 'desc')->limit(5)->get()
+            'review' => RestaurantReview::where('restaurant_id', $restaurant->id)->orderBy('created_at', 'desc')->limit(5)->get(),
+            'visitor' => $result,
+            'tahun' => $fiveYearsAgo
         ];
         return view('front.pages.restaurant.show',  $data);
     }
--- app/Http/Controllers/Web/Front/TourismController.php
+++ app/Http/Controllers/Web/Front/TourismController.php
@@ -30,32 +30,29 @@ class TourismController extends Controller
     
         // Ambil tahun dari request jika tersedia, atau gunakan sekarang sebagai default
         $tahun = $request->input('tahun', date('Y'));
-    
         $visitorData = TourismVisitor::where('tourism_id', $tourism->id)
             ->whereYear('date', $tahun)
             ->get();
-    
         $monthlyVisitor = [];
         for ($i = 1; $i <= 12; $i++) {
             $monthlyVisitor[$i] = 0;
         }
-    
         foreach ($visitorData as $visitor) {
             $month = date('n', strtotime($visitor->date)); 
             $monthlyVisitor[$month] += (int)$visitor->visitor;
         }
         $result = array_values($monthlyVisitor);
-    
-        $viewer = new TourismViewer();
-        $viewer->tourism_id = $tourism->id;
-        $viewer->user_id = Auth::user()->id ?? null;
-        $viewer->save();
-    
+
         // Array 5 tahun kebelakang
         $fiveYearsAgo = [];
         for ($i = 0; $i < 5; $i++) {
             $fiveYearsAgo[] = (int)date('Y', strtotime("-$i year"));
         }
+    
+        $viewer = new TourismViewer();
+        $viewer->tourism_id = $tourism->id;
+        $viewer->user_id = Auth::user()->id ?? null;
+        $viewer->save();
     
         $data = [
             'title' => 'Wisata',
